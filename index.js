3ed196858648c9ee627f95616647c231ce9d1928
jasminJoachims <61379075+jasminJoachims@users.noreply.github.com>
2022-08-13 12:50:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+///document.querySelectorAll("button").addEventListener("click", function() {
+///  alert("I got clicked!");
+///});
+
+
+///const buttons = document.querySelectorAll("button");
+
+var crash = new Audio("sounds/crash.mp3");
+var kickbass = new Audio("sounds/kick-bass.mp3");
+var snare = new Audio("sounds/snare.mp3");
+var tom1 = new Audio("sounds/tom-1.mp3");
+var tom2 = new Audio("sounds/tom-2.mp3");
+var tom3 = new Audio("sounds/tom-3.mp3");
+var tom4 = new Audio("sounds/tom-4.mp3");
+
+///buttons.forEach((item) => {
+///  item.addEventListener("click", function() {
+///    crah.play();
+///  })
+///})
+
+///keydown event listener instead of keypress
+
+for(var i=0; i<7; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var button = this.innerHTML;
+    makeSound(button);
+
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  var button = event.key;
+  makeSound(button);
+})
+
+function makeSound(button) {
+  switch (button) {
+    case "w":
+      tom1.play();
+      break;
+    case "a":
+      tom2.play();
+      break;
+    case "s":
+      tom3.play();
+      break;
+    case "d":
+      tom4.play();
+      break;
+    case "j":
+      snare.play();
+      break;
+    case "k":
+      kickbass.play();
+      break;
+    case "l":
+      crash.play();
+      break;
+    default: console.log(button);
+
+  }
+}
